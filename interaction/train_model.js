e1a9c665334cf21fb29818f9f2b09863dd8cf13c
Jannis Spiekermann <janniss@hotmail.de>
2021-03-17 13:40:30 +0100

    Add live status of id and file name

--- interaction/train_model.js
+++ interaction/train_model.js
@@ -10,11 +10,28 @@ function doesConnectionExist(status) {
     }
   }
   
+
 function saveModelId() {
     modelId = document.getElementById('trainId').value;
-    alert("The current model ID is: " + modelId);
     console.log("model ID is: " + modelId);
  }
+
+
+function setCurrentId() {
+    document.getElementById("currentId").innerHTML = modelId;
+}
+
+
+ function saveFileName() {
+    fileName = document.getElementById('trainFile').value;
+    console.log("You are using the file: " + fileName);
+ }
+
+ 
+function setCurrentFileName() {
+    document.getElementById("currentFileName").innerHTML = fileName;
+}
+
 
 // TODO: ShortAnswerInstance must be created.
 // TODO: Input must be stored in ShortAnswerInstances and then passed to this function.
@@ -38,12 +55,22 @@ function trainFromAnswers(modelId, uploadedInstances) {
     });
   }
 
+
 // Initialize modelId variable for usage.
 var modelId = null;
+var fileName = null;
 
 // Store the modelId on click.
-document.getElementById("trainIdButton").onclick = function() {saveModelId()};
+document.getElementById("trainIdButton").onclick = function() {saveModelId(), setCurrentId()};
+
+// Store the file name on click.
+document.getElementById("trainFileButton").onclick = function() {saveFileName(), setCurrentFileName()};
+
+// TODO: Extract the information and create ShortAnswerInstances.
+//       Add the instances variable to the trainFromAnswers function below.
 
 var respObj;
 var respStatus;
-trainFromAnswers();
+// Store the file name on click.
+document.getElementById("trainButton").onclick = function() {trainFromAnswers(modelId)};
+
